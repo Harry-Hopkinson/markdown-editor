68acfafbc8b6c7716419eec799e6689c56d20256
Harry-Hopkinson <harryjhopkinson@gmail.com>
2022-01-18 20:24:23 +0000

    Project now uses React instead of Vue

--- packages/renderer/vite.config.js
+++ packages/renderer/vite.config.js
@@ -3,14 +3,9 @@
 import {chrome} from '../../.electron-vendors.cache.json';
 import {join} from 'path';
 import {builtinModules} from 'module';
-import vue from '@vitejs/plugin-vue';
 
 const PACKAGE_ROOT = __dirname;
 
-/**
- * @type {import('vite').UserConfig}
- * @see https://vitejs.dev/config/
- */
 const config = {
   mode: process.env.MODE,
   root: PACKAGE_ROOT,
@@ -19,7 +14,7 @@ const config = {
       '/@/': join(PACKAGE_ROOT, 'src') + '/',
     },
   },
-  plugins: [vue()],
+  plugins: [],
   base: '',
   server: {
     fs: {
---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+const options = {
+    singleQuote: false,
+    bracketSpacing: false,
+    semi: false,
+    tabWidth: 4,
+    trailingComma: "none"
+}
+
+module.exports = options;
